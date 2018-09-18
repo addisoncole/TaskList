29bc535db8caa7cab802514c4931a8f3b9925490
Addison Cole <addison.cole85@gmail.com>
2018-09-18 14:52:57 -0700

    wave 1 work, create new tasks and modify. db:rollback to change completion_date to string rather than date instance.

--- db/migrate/20180918182623_create_tasks.rb
+++ db/migrate/20180918182623_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :action
       t.string :description
-      t.date :completion_date
+      t.string :completion_date
       t.timestamps
     end
   end
