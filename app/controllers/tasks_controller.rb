925da1868d9e8b337e86d74857aa3034b97a3e83
Addison Cole <addison.cole85@gmail.com>
2018-09-22 16:34:06 -0700

    add method to add a new task, set up view and create link on index to link to form to create new task entry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,44 @@
 
 class TasksController < ApplicationController
   def index
-
+    @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+  filtered_task_params = task_params()
+  @task = Task.new(filtered_task_params)
+
+  is_successful_save = @task.save
+
+  if is_successful_save
+    redirect_to tasks_path
+  else
+    render :new
+  end
+end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @tasks = Task.all
+    @task = @tasks[task_id]
     if @task.nil?
       head :not_found
     end
   end
+
+  private
+
+  def task_params
+  return params.require(:task).permit(
+    :action,
+    :description,
+    :completion_date,
+    :assignee
+  )
+end
 
 end
