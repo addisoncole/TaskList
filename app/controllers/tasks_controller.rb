7906d23e53e7cfdf08c9b45862d5d0ec219a06a5
Addison Cole <addison.cole85@gmail.com>
2018-09-23 15:47:01 -0700

    css styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,36 +9,57 @@ class TasksController < ApplicationController
   end
 
   def create
-  filtered_task_params = task_params()
-  @task = Task.new(filtered_task_params)
+    filtered_task_params = task_params()
+    @task = Task.new(filtered_task_params)
 
-  is_successful_save = @task.save
+    is_successful_save = @task.save
 
-  if is_successful_save
-    redirect_to tasks_path
-  else
-    render :new
+    if is_successful_save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
-end
 
   def show
-    task_id = params[:id].to_i
-    @tasks = Task.all
-    @task = @tasks[task_id]
+    @task = Task.find_by(id: params[:id])
     if @task.nil?
       head :not_found
     end
   end
 
+  def edit
+    @task = Task.find_by(id: params[:id])
+  end
+
+  def update
+    filtered_task_params = task_params()
+    @task = Task.find(params[:id])
+    is_successful_update = @task.update(filtered_task_params)
+
+    if is_successful_update
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-  return params.require(:task).permit(
-    :action,
-    :description,
-    :completion_date,
-    :assignee
-  )
-end
+    return params.require(:task).permit(
+      :action,
+      :description,
+      :completion_date,
+      :assignee
+    )
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
     get '/tasks/new', to: 'tasks#new', as: 'new_task'
     post '/tasks', to: 'tasks#create'
 
-    get '/tasks/:id', to: 'tasks#show'
+    get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+    patch '/tasks/:id', to: 'tasks#update'
+
+    delete '/tasks/:id', to: 'tasks#destroy'
+
+    get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 end
