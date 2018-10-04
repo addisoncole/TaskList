c34ef7fad32a9f9e1eb0c6eb22a8d7b25ef7efb7
Addison Cole <addison.cole85@gmail.com>
2018-10-04 10:13:15 -0700

    heroku and add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get '/tasks', to: 'tasks#index', as: 'tasks'
 
-    get '/tasks/new', to: 'tasks#new', as: 'new_task'
-    post '/tasks', to: 'tasks#create'
+  root 'tasks#index'
 
-    get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
 
-    patch '/tasks/:id', to: 'tasks#update'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
 
-    post '/tasks/:id/complete', to: 'tasks#toggle_complete', as: 'task_toggle_complete'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-    delete '/tasks/:id', to: 'tasks#destroy'
+  patch '/tasks/:id', to: 'tasks#update'
 
-    get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  post '/tasks/:id/complete', to: 'tasks#toggle_complete', as: 'task_toggle_complete'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
 end
